1d171ce55151715506f7fad261fc90900707dd95
chupsu <chupsu@mail.ru>
2020-12-08 02:58:44 +0300

    end catalog grid

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,5 @@
 $(function () {
+  // Slider
   $('.slider__wrapper').slick({
      arrows: false,
      dots: true,
@@ -7,14 +8,47 @@ $(function () {
      autoplay: true,
      autoplaySpeed: 2000
   });
+  // Range
+  $('.filter__range').ionRangeSlider({
+    type: "double",
+    onStart: function (data){
+      $('.filter__range-from').text('$' + data.from + '.00');
+      $('.filter__range-to').text('$' + data.to + '.00');
+    },
+    onChange: function (data){
+      $('.filter__range-from').text('$' + data.from + '.00');
+      $('.filter__range-to').text('$' + data.to + '.00');
+    },
+  });
+  // Star Rate
+  $('.star').rateYo({
+    starWidth: '11px',
+    normalFill: '#d6d6d6',
+    ratedFill: '#ffcc00',
+    readOnly: true
+  });
+  // View Catalog
+  $('.filter__view-btn').on('click', function(){
+    $('.filter__view-btn').removeClass('filter__view-btn--active');
+    $(this).addClass('filter__view-btn--active');
+  });
+  $('.btn-list').on('click', function(){
+    $('.product__item').addClass('.--list');
+  })
+  $('.btn-grid').on('click', function(){
+    $('.product__item').removeClass('.--list');
+  })
+  // Style Select
+  $('.filter__quantity-style').styler();
 });
-let mixer1 = mixitup('.week-product__items', {
+// Tabs
+let mixer1 = mixitup('.product__items', {
   selectors: {
     control: '.week__filter-btn'
   }
 });
-let mixer2 = mixitup('.new-product__items', {
+let mixer2 = mixitup('.design__items', {
   selectors: {
-    control: '.new__filter-btn'
+    control: '.design__filter-btn'
   }
 });
